f6c6653c9546c7a58146f0a802cad90f68858694
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-09-02 10:03:32 +0200

    DP-1101: Add status component

---
+++ src/components/DatasetList/DatasetStatus.vue
@@ -0,0 +1,79 @@
+<!---Show datasetstatus, FAILED or OK 
+    TODO: Need to connect status with correct dataset 
+--->
+<template>
+  <div class="dataset-status">
+    <IconSpinner v-if="!finished" />
+    <div v-else>
+      <IconCheckCircle v-if="!failed" />
+      <IconFailedCircle v-else />
+    </div>
+    <span v-if="!failed">OK</span>
+    <span v-else>FEIL</span>
+  </div>
+</template>
+<script>
+import IconFailedCircle from '@/components/icons/IconFailedCircle'
+import IconSpinner from '@/components/icons/IconSpinner'
+import IconCheckCircle from '@/components/icons/IconCheckCircle'
+import datasetstatus from '@/assets/mock/status.json'
+export default {
+  name: 'DatasetStatus',
+  components: {
+    IconFailedCircle,
+    IconCheckCircle,
+    IconSpinner,
+  },
+  data() {
+    return {
+      datasetstatus,
+    }
+  },
+  computed: {
+    /**
+     * There are 3 ways that shows that the processing is finished:
+     * If status = FINISHED
+     * If there is a date_end
+     * If the status is FAILED
+     */
+    finished() {
+      if (this.runStatus) {
+        return true
+      }
+      if (this.datasetstatus.date_end !== 'N/A') {
+        return true
+      }
+      if (this.failed) {
+        return true
+      }
+      return false
+    },
+    failed() {
+      if (this.datasetstatus.status === 'FAILED') {
+        return true
+      }
+      return false
+    },
+    runStatus() {
+      if (this.datasetstatus.run_status === 'FINISHED') {
+        return true
+      }
+      return false
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/variables';
+.dataset-status {
+  display: inline-flex;
+}
+.IconCheckCircle {
+  color: $green;
+  margin-right: 5px;
+}
+.IconFailedCircle {
+  color: $red;
+  margin-right: 5px;
+}
+</style>
