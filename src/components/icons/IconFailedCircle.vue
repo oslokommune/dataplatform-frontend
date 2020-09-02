bb283cce2bd952411b02437adf7f569e92c8bd48
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-09-02 10:02:06 +0200

    DP-1101: Add spinner and add contrastcolor to icons - UU

--- src/components/icons/IconFailedCircle.vue
+++ src/components/icons/IconFailedCircle.vue
@@ -1,6 +1,15 @@
 <template>
-  <svg class="IconFailedCircle" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <path fill="none" d="M0 0h24v24H0V0z"/>
-    <path d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41L15.59 7z" fill="currentColor"/>
+  <svg
+    class="IconFailedCircle"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <circle cx="12" cy="12" r="8" fill="#292858" />
+    <path
+      d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41L15.59 7z"
+      fill="currentColor"
+    />
   </svg>
 </template>
---
+++ src/components/icons/IconSpinner.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    class="IconSpinner"
+    height="29px"
+    preserveAspectRatio="xMidYMid"
+    style="background: none"
+    viewBox="0 0 100 100"
+    width="29px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="50"
+      cy="50"
+      fill="none"
+      r="35"
+      stroke="#6EE9FF"
+      stroke-dasharray="164.93361431346415 56.97787143782138"
+      stroke-width="10"
+      transform="rotate(147.239 50 50)"
+    >
+      <animateTransform
+        attributeName="transform"
+        begin="0s"
+        calcMode="linear"
+        dur="1s"
+        keyTimes="0;1"
+        repeatCount="indefinite"
+        type="rotate"
+        values="0 50 50;360 50 50"
+      />
+    </circle>
+  </svg>
+</template>
