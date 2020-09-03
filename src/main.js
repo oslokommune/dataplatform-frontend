160ff51b4c3fe6b08095bf461206f72d725807fe
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-09-03 13:31:44 +0200

    DP-1077 Add basic authentication module (POC stage)
    Add a module for logging in a Keycloak user through Gatekeeper.
    
    Currently at an early POC-stage to build upon later.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
+import * as authenticationStore from './store/authentication'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    auth: {
+      namespaced: true,
+      ...authenticationStore,
+    },
+  },
+})
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app')
---
+++ src/store/authentication.js
@@ -0,0 +1,111 @@
+/**
+ * Store for keeping track of the current authentication status.
+ *
+ * Also provides actions for logging in, logging out, and refreshing an already
+ * authenticated user.
+ */
+
+/*
+ * TODO: Make axios available on $this.axios so we don't have to
+ * import it everywhere.
+ */
+import axios from 'axios'
+
+export const state = () => ({
+  user: null,
+})
+
+export const getters = {
+  isAuthenticated(state) {
+    return state.user !== null
+  },
+  loggedInUser(state) {
+    return state.user
+  },
+}
+
+export const mutations = {
+  resetUser(state) {
+    state.user = null
+  },
+  setUser(state, user) {
+    state.user = user
+  },
+}
+
+export const actions = {
+  async refreshUser({ commit, getters }) {
+    if (getters['isAuthenticated']) {
+      return getters['loggedInUser']
+    }
+
+    try {
+      const { data } = await axios.request({
+        // TODO: Fetch this from an environment variable.
+        baseURL: 'http://localhost:4554',
+        url: '/userinfo',
+        method: 'get',
+        withCredentials: true,
+      })
+
+      commit('setUser', data)
+    } catch (error) {
+      if (error.response) {
+        switch (error.response.status) {
+          case 400:
+            if (process.server) {
+              // TODO: Fetch this from an environment variable
+              console.error(
+                `Couldn't reach Gatekeeper on url http://localhost:4554`
+              )
+            }
+            throw error
+          case 401:
+            return null // Ignore 401, it's expected due to many users not being logged in
+          default:
+            throw error
+        }
+      }
+    }
+
+    return getters['loggedInUser']
+  },
+  async login(context, redirectTo) {
+    if (!redirectTo) {
+      redirectTo = this.$env.BASE_URL + this.$router.history.current.fullPath
+    }
+
+    const encodedRedirectUrl = encodeURIComponent(redirectTo)
+    const url =
+      this.$env.GATEKEEPER_URL + `/login?redirect=${encodedRedirectUrl}`
+
+    await handleRedirect(this.app.context.redirect, url)
+  },
+  async logout({ commit }) {
+    await axios.request({
+      method: 'post',
+      url: this.$env.GATEKEEPER_URL + '/logout',
+      withCredentials: true,
+    })
+
+    commit('resetUser', null)
+
+    await handleRedirect(this.app.context.redirect, this.app.$env.BASE_URL)
+  },
+}
+
+async function handleRedirect(redirectFn, target) {
+  try {
+    redirectFn(target)
+  } catch (error) {
+    if (error.message !== 'ERR_REDIRECT') throw error
+    /*
+     * If client side, the redirect function throws an error. In this event, we have to
+     * stall until the actual window.location.replace happens.
+     * This promise will in 99.9% of all cases never resolve due to redirection occurring
+     */
+    await new Promise((resolve) => {
+      window.setTimeout(() => resolve(), 60000)
+    })
+  }
+}
