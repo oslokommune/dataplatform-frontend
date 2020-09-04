9607fc0ba5eede28b271845064f2df69f8a4a368
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-09-04 14:10:47 +0200

    Remove eslint rule: no console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-	root: true,
+  root: true,
 
-	env: {
-		node: true
-	},
+  env: {
+    node: true,
+  },
 
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-	},
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 
-	parserOptions: {
-		parser: 'babel-eslint',
-	},
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 
-	extends: ['plugin:vue/essential', '@vue/prettier', 'eslint:recommended']
+  extends: ['plugin:vue/essential', '@vue/prettier', 'eslint:recommended'],
 }
