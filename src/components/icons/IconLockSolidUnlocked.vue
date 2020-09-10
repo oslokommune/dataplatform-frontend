0b8078a1489b87dd26165e68d338b81bc3153f43
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2020-09-10 16:10:48 +0200

    Add dataset details page
    Related task:
    https://jira.oslo.kommune.no/browse/DP-1160

---
+++ src/components/icons/IconLockSolidUnlocked.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 470 470"
+    style="enable-background: new 0 0 470 470"
+    xml:space="preserve"
+    height="16"
+    width="16"
+    class="IconLockSolidUnlocked"
+  >
+    <g>
+      <rect x="137.5" y="190" width="315" height="280" />
+      <path
+        d="M192.5,102.5V160h30v-57.5C222.5,45.981,176.519,0,120,0S17.5,45.981,17.5,102.5V190h30v-87.5
+		C47.5,62.523,80.023,30,120,30S192.5,62.523,192.5,102.5z"
+      />
+    </g>
+  </svg>
+</template>
---
+++ src/modules/Dataset/Editions.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="dataset-editions">
+    <div v-if="error">
+      <div v-if="error === 'notFound'">Kunne ikke finne utgivelser.</div>
+    </div>
+
+    <p v-if="!loading && editions && editions.length === 0">
+      Ingen utgivelser.
+    </p>
+
+    <table v-if="editions && editions.length > 0">
+      <thead>
+        <tr>
+          <th>Beskrivelse</th>
+          <th>Opprettet</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="edition in editions" :key="edition.Id">
+          <td>{{ edition.description || edition.Id }}</td>
+          <td>{{ formatDate(edition.createdAt) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import format from 'date-fns/format'
+
+import env from '@/utils/env'
+
+const sortAscending = (field) => (a, b) => {
+  return b[field] - a[field]
+}
+
+export default {
+  name: 'Editions',
+  props: {
+    datasetId: String,
+  },
+  data() {
+    return {
+      editions: [],
+      error: null,
+      loading: false,
+    }
+  },
+  created() {
+    this.fetchEditions()
+  },
+  watch: {
+    datasetId: 'fetchEditions',
+  },
+  methods: {
+    formatDate(date) {
+      if (!date) {
+        return ''
+      }
+      return format(date, "dd.MM.yyyy, 'kl.' HH.mm")
+    },
+    async fetchEditions() {
+      this.error = null
+      this.editions = []
+
+      if (this.datasetId) {
+        this.loading = true
+
+        const { data: editions } = await axios.request({
+          baseURL: env.VUE_APP_API_DATAPLATFORM_BASE_URL,
+          url: `/metadata/datasets/${this.datasetId}/versions/1/editions`,
+          method: 'get',
+          withCredentials: true,
+        })
+
+        this.loading = false
+        if (!editions) {
+          this.error = 'notFound'
+        } else {
+          const parsedEditions = editions.map((edition) => ({
+            ...edition,
+            createdAt: edition.edition ? new Date(edition.edition) : 0,
+          }))
+
+          const parsedAndSortedEditions = parsedEditions.sort(
+            sortAscending('createdAt')
+          )
+
+          this.editions = parsedAndSortedEditions
+        }
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/variables';
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+
+  th {
+    border-bottom: 2px solid black;
+    padding: 0.75em 1em 1.3em 0;
+    text-align: left;
+  }
+
+  td {
+    border-bottom: $separator-border;
+    padding: 1.3em 1em 1.3em 0;
+  }
+}
+</style>
