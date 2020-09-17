97fa64989d80fe0162a8c4237446f251266847ab
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2020-09-17 09:52:32 +0200

    ✨ Add alert component
    Variants:
    * danger
    * info
    * muted
    * success
    * warning
    
    Related task:
    https://jira.oslo.kommune.no/browse/DP-1189

---
+++ src/components/Alert/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="alert" :class="`alert--${variant}`" role="alert">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: {
+    variant: {
+      default: 'info',
+      validator: (value) =>
+        ['danger', 'info', 'muted', 'success', 'warning'].includes(value),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  font-weight: 500;
+  margin-bottom: 1rem;
+  padding: 1rem 2rem;
+
+  strong {
+    font-weight: 800;
+  }
+
+  a {
+    color: inherit;
+  }
+
+  &--danger {
+    color: $ok-state-danger-text;
+    background-color: $ok-state-danger-bg;
+  }
+  &--info {
+    color: $ok-state-info-text;
+    background-color: $ok-state-info-bg;
+  }
+  &--muted {
+    color: $ok-state-muted-text;
+    background-color: $ok-state-muted-bg;
+  }
+  &--success {
+    color: $ok-state-success-text;
+    background-color: $ok-state-success-bg;
+  }
+  &--warning {
+    color: $ok-state-warning-text;
+    background-color: $ok-state-warning-bg;
+  }
+}
+</style>
--- src/modules/StyleOverview.vue
+++ src/modules/StyleOverview.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <h1>Styles</h1>
+
     <h2>Media queries</h2>
 
     <h3>Only show elements on specified screen sizes</h3>
+
     <p>
       The classes sm, md and lg will make the element only show on that screen
       size:
@@ -17,13 +19,49 @@
     <br />
 
     <h3>Toggle different css for different screen sizes</h3>
+
     <div class="custom">Custom</div>
+
+    <br />
+    <br />
+
+    <h2>Alert</h2>
+
+    <Alert variant="danger">
+      <strong>Oh my!</strong> Lorem ipsum dolor sit danger, consectetur
+      <a href="#">adipiscing</a> elit.
+    </Alert>
+
+    <Alert variant="info">
+      <strong>Oh my!</strong> Lorem ipsum dolor sit info, consectetur
+      <a href="#">adipiscing</a> elit.
+    </Alert>
+
+    <Alert variant="muted">
+      <strong>Oh my!</strong> Lorem ipsum dolor sit muted, consectetur
+      <a href="#">adipiscing</a> elit.
+    </Alert>
+
+    <Alert variant="success">
+      <strong>Oh my!</strong> Lorem ipsum dolor sit success, consectetur
+      <a href="#">adipiscing</a> elit.
+    </Alert>
+
+    <Alert variant="warning">
+      <strong>Oh my!</strong> Lorem ipsum dolor sit warning, consectetur
+      <a href="#">adipiscing</a> elit.
+    </Alert>
   </div>
 </template>
 
 <script>
+import Alert from '@/components/Alert'
+
 export default {
   name: 'StyleOverview',
+  components: {
+    Alert,
+  },
 }
 </script>
 
