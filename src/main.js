dc32c2efcb23a2be33f37e5c73feb3e1f58d8603
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2021-03-23 14:41:32 +0100

    Test

--- src/main.js
+++ src/main.js
@@ -15,6 +15,17 @@ Vue.config.productionTip = false
 Vue.use(analyticsPlugin)
 Vue.use(axiosVuePlugin)
 
+function endsWith(x, y) {
+  return x.lastIndexOf(y) === x.length - y.length
+}
+
+console.log(endsWith(['1', '2', '3'], '2'))
+console.log(endsWith(['1', '2', '3'], '3'))
+
+const regex = new RegExp('(^s*)my-marker(s*$)')
+const isMyMarkerText = regex.test('asdfasdfasdf')
+console.log(isMyMarkerText)
+
 new Vue({
   router,
   store,
